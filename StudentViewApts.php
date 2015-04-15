fecaeccebebcf8241edfb4fdee0a42fa1eb1525f
Kyle44 <kyle44@comcast.net>
2015-04-14 23:32:50 -0400

    Update StudentViewApts.php

--- StudentViewApts.php
+++ StudentViewApts.php
@@ -1,6 +1,7 @@
 // File: StudentViewApts.php
 // Author: KyleFritz
 // Date Created: 4/11/2015
+// Last Modified: 4/14/2015
 // Description: This page will only come up if the last page was StudentOptions.php 
 //   and "View Created Appointment" was created.
 
@@ -28,17 +29,24 @@ else{
 	$sql = "SELECT `dateTime` FROM `Advising_Appointments2` WHERE `studentId` LIKE '$studentId'";
 	$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
 	
+	
+	/**********  Not sure if all of this is useless now
+
 	// Set up variables to equal Session variables
 	$date = $_SESSION['date'];
   	$time = $_SESSION['time'];
   	$sqlFormatDate = date('Y-m-d', $date);
-  	$sqlFormatTime = date('h:i:s', $time);
+	// Changed from h:i:s to H:i:s to allow for a 24 hour clock
+  	$sqlFormatTime = date('H:i:s', $time);
 	
+	***********************/
+	
+
 
 	// set the default timezone to use.
 	date_default_timezone_set('EST');
-	// Not sure if this works, but I believe this will give me the date and time of right now.
-	$dateAndTime = date('Y-m-d h:i:s');
+	// Not sure if this works, but I believe this will give me the date and time of right now. Changed h:i:s to H:i:s.
+	$dateAndTime = date('Y-m-d H:i:s');
 
 	// Create two arrays, one for past appointments (ones that have already occurred) and one for upcoming appointments.
 	$pastApts = array();
@@ -88,7 +96,7 @@ else{
 
 
 
-	<!--  THE BUTTONS "Go Back" and "Done".  I don't think that I need 2 buttons here when they should go to the same place.  -->
+	<!--  THE BUTTONS "Go Back" and "Done".  -->
 
 	<!-- Post method to go to StudentOptions.php.  Name means StudentViewApts.php (this page) to StudentOptions.php  -->
 	<form action='StudentOptions.php' method='post' name='SVAtoSOptions'>
@@ -97,8 +105,8 @@ else{
 	<!-- End of form  -->
 	</form>
 
-	<!-- Post method to go to index.php.  Name means StudentViewApts.php (this page) to StudentOptions.php  -->
-	<form action='StudentOptions.php' method='post' name='SVAtoINDEX'>
+	<!-- Post method to go to index.php.  Name means StudentViewApts.php (this page) to index.php  -->
+	<form action='index.php' method='post' name='SVAtoINDEX'>
 	<!-- Done button  -->
 	<input type='Done'>
 	<!-- End of form -->

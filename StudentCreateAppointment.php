3a8ae8aa0813de8139ce614df9ea1ce120116126
bnat1 <bnat1@umbc.edu>
2015-04-16 15:17:14 -0400

    Update StudentCreateAppointment.php

--- StudentCreateAppointment.php
+++ StudentCreateAppointment.php
@@ -1,20 +1,22 @@
 <?php
 session_start();
+
 /*
 Name: Nathaniel Baylon
 Date:03/21/2015
 Class: CMSC331
 Project:Advisor Time Selection
 File: StudentCreateAppointmnet.php
-File Description: This file allows student to choose a time. I will have to 
-                  only let the student choose certain times for each major
+File Description: 
+In this file, a student chooses a radio button for the day they want to sign up
+for, and selects a time from a select box on that same line. Times only show up
+within 2 business days to a week after that if the current week is enabled, or 
+next monday through the following monday if current week is disabled. If day has 
+no availabilities, the student cannot select it. 
 */
+
 include ('Proj2Head.html');
 include('../CommonMethods.php');
-
-//get datetime from Advising_Availability for $_SESSION['studentsAdvisor']
-//they might not be in order :( so I will have to sort them...
-
 
 $advisorId = $_SESSION['studentsAdvisor'];
 $advisorName = $_SESSION['advisors'][$advisorId];
@@ -22,7 +24,9 @@ $advisorName = $_SESSION['advisors'][$advisorId];
 //echo "$advisorId<br>";
 $fName = $_SESSION['fName'];
 
-$todayDOW = date(l);//day of week today
+//defining date range the student can select from
+//today's day of week
+$todayDOW = date(l);
 if($_SESSION['currentWeekEnabled']){
 
 	//weekdays
@@ -41,7 +45,7 @@ if($_SESSION['currentWeekEnabled']){
 	else if($todayDOW == "Friday" || $todayDOW == "Saturday" || $todayDOW == "Sunday"){
 		$startDate = strtotime('Tuesday');
 	}
-	$endDate = strtotime("+1 week +1 day", $startDate);
+	$endDate = strtotime("+1 week", $startDate);
 }
 
 else{
@@ -54,24 +58,25 @@ $sqlFormatStartDate = date("Y-m-d H:i:s",$startDate);
 $sqlFormatEndDate = date("Y-m-d H:i:s",$endDate);
 //echo "Testing start and end date: $sqlFormatStartDate"."$sqlFormatEndDate<br>";
 
+//instantiate common
 $debug = false;
 $COMMON = new Common($debug);
+
+//get available apts between start/end time with the previously selected advisor
 $sql = "SELECT * FROM Advising_Availability2 where `advisorId` = '$advisorId' 
 		AND `dateTime` BETWEEN '$sqlFormatStartDate' AND '$sqlFormatEndDate'";
 $rs = $COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
 
+//push into array (all of the appointments in Advising_Avialability w/i that
+//time range)
 $allPotentialAppointmentArray = array();
-
 while($row = mysql_fetch_assoc($rs)){
 	array_push($allPotentialAppointmentArray, $row);
-//echo "o";
-	//has all appointments within that timeframe with that advisor
 }
 
-//make sure there are openings, based on whether an appointmnet is taken in
-//new addition: numSlots in Advising_Availability2 has number of slots in an appt
-//Advising_Appointmnets, looping through each appointment in $allPotentialAppointmentArray
-
+//numSlots in Advising_Availability2 has number of slots in an appt
+//avavailbleAppointmentArray holds times that are available to sign up for, eg,
+//number of taken slots does not exceed the number of slots in the appoinmtnet time
 $availableAppointmentArray = array();
 foreach($allPotentialAppointmentArray as $row){
 	$sqlFormatTime = $row["dateTime"];
@@ -90,26 +95,26 @@ foreach($allPotentialAppointmentArray as $row){
 	if($numSlots >$numTakenSlots){
 		array_push($availableAppointmentArray, $sqlFormatTime);
 	}
-//echo "a";
 }
-//now, I have just times that are available, and will show as options for the student
 
 //get a list of all possible days, to display. If no times are available in that day, 
-//it will say so in the output
-
 //var_dump($availableAppointmentArray);
-
+//counter is a looping variable that holds a date
+//outputArray holds dates and times that will be displayed, with the day as the key
 	$outputArray = array();
 	$counter = $sqlFormatStartDate;
 	//echo "sqlFormatStartDate: $sqlFormatStartDate";
-	//in sql format
+	
+	//go up to, but do not include end date (end date is beginning of day after last day 
+	//that will show up
 	while($counter != $sqlFormatEndDate){
-		//echo "$counter $sqlFormatEndDate<br>";
+		//userFormatDate: date that will be seen
 		$userFormatDate = date("l, m/d/Y",strtotime($counter));
 		if(date('l', strtotime($userFormatDate))!= "Saturday" && 
 			date('l', strtotime($userFormatDate))!= "Sunday"){
 			$outputArray[$userFormatDate] = array('No available appointment times');
 		}
+		//increment counter
 		$counter = date("Y-m-d H:i:s", strtotime("+1 days", strtotime($counter)));
 	}
 	//echo "output Array: ";
@@ -125,10 +130,7 @@ foreach($allPotentialAppointmentArray as $row){
 	//echo "$availableDateTime<br>";
 	$availableDate = date("l, m/d/Y", strtotime($availableDateTime));
 	$availableStartTime = date("g:i a", strtotime($availableDateTime));
-	//$availableEndTime = date("g:i a", strtotime("+30 minutes", strtotime($availableDateTime)));
-	$availableTime = $availableStartTime;//."-".$availableEndTime;
-	//echo "available date: $availableDate availableTime: $availableTime<br>";
-	array_push($outputArray[$availableDate], $availableTime);
+	array_push($outputArray[$availableDate], $availableStartTime);
 	} 
 
 	//var_dump($outputArray);
@@ -145,39 +147,48 @@ foreach($allPotentialAppointmentArray as $row){
 	}
 	echo "Today is $today.<br><br>";
 	
+	//first day is selected by default
 	$previousButton = False;
 	echo "<form action = 'StudentAreYouSure.php' method = 'post'>";
 	foreach($outputArray as $userDay=>$times){
+		//at least one real time in that day
 		if(sizeof($times) > 1){
 			$intDay = strtotime($userDay);
-			
+	
+			//radio button
 			echo "<input type = 'radio' name = 'date' value = '$intDay'";
-		if(!$previousButton){echo' checked';} 
-			echo ">$userDay: ";
-		$previousButton = true;
+			if(!$previousButton){echo' checked';} 
+				echo ">$userDay: ";
+			$previousButton = true;
+
+			//select boxes are stored by day in post
 			echo "<select name = '$intDay'>";
-			foreach($times as $userTime){
-				$intTime = strtotime($userTime);
-				if($userTime != 'No available appointment times'){
-					echo "<option value = '$intTime'>$userTime</option>";
+			foreach($times as $startTime){
+				//display as: "g:i-g:i a" for user
+				if($startTime != 'No available appointment times'){
+					$endTime = date("g:i a", strtotime("+30 minutes", strtotime($startTime)));
+					$availableTime = $startTime."-".$endTime;
+					$intTime = strtotime($startTime);
+					echo "<option value = '$intTime'>$availableTime</option>";
 				}
-			}
-			echo "<br>";	
+			}	
 		}//end if
 	
 		else{
+			//No available appointments
 			echo "$userDay: $times[0]";
 		}//end else
 	echo "</select><br>";
 	}//end foreach	
+	echo"<br>";
 
-
+//do not include next button if no appointments
 $disableNext=false;
 if(empty($availableAppointmentArray)){	
 	$disableNext = true;
 	if( $advisorName == 'Group Advising'){
 		echo "Sorry, there are no available Group Advising appointments at this time. 
-			Please try again tomorrow, or choose a different advising option.<br>";
+			Please try again later, or choose a different advising option.<br>";
 	}
 	else{
 		echo "Sorry $fName, $advisorName has no available appointments at this time.
@@ -185,7 +196,7 @@ if(empty($availableAppointmentArray)){
 	}
 }
 ?>
-<input type = 'submit' value = 'Next'<?php if($disableNext){echo "disabled";}?> >
+<input type = 'submit' value = 'Next'<?php if($disableNext){echo "hidden";}?> >
 	</form>
 <form action='StudentOptions.php'>
 	<input type = 'submit' value='Go Back'>

c0197845c41439314ab4c8caac53769bb91d3e28
Kyle44 <kyle44@comcast.net>
2015-04-22 11:10:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,81 +1,34 @@
-<html>
-	<head>
-<!--
+<?php
+session_start();
+session_unset();
+/*
 Name: Nathaniel Baylon
 Date:03/29/2015
 Class: CMSC331
-Project:Project 2
+Project:Project2
+File: index.php
+File Description: This is the welcome page, where the user can choose either to start an
+advisor session, or a student session 
+*/
+include('Proj2Head.html');
+?>
 
-File: Proj2Head.html
-File Description: This file contains the head that will be included throughout the
-				  project. The head has the CSS selectors for styling, and the UMBC 
-				  logo appears as the first element displayed on each page.
--->
+<!--output-->
+Welcome<br>
 
-<title>Advising Scheduling</title>
-<link rel="icon" type="image/png" href="http://sites.umbc.edu/wp-content/themes/umbc/assets/images/icon.png" />
-<link rel="stylesheet" type="text/css" href="css/stylesheet.css"/>
-</head>
-<body>
-<div class="one-third-header"></div>
-<div class="two-third-header">
-<img class="logo" src="picture/Logo.png" alt="UMBC Advising">
-</div>
-<div class="three-third-header"></div>
-<div class="background">
-<div class="one-third-background"></div>
-<div class="two-third-background"></div>
-<div class="three-third-background"></div>
-</div>
-<!--rest of php/html code below, /body and /html in Proj2Tail.html-->
+<!--buttons-->
+
+<form action='AdvisorSignin.php' value='Advisor'>
+	<input type='submit' value='Advisor'>
+</form>
+
+<form action='StudentSignin.php' value='Student'>
+	<input type='submit' value='Student'>
+</form>
 
 
-<!--links-->
 
-<div class="one-third-nav"></div>
-<div class="two-third-nav">
-<div class="index-advisor-nav">
-<div class="nav-div"><a class="nav" href="AdvisorSignin.php">Advisor</a></div>
-</div>
-<div class="index-student-nav">
-<div class="nav-div"><a class="nav" href="StudentSignin.php">Student</a></div>
-</div>
-</div>
-<div class="three-third-nav"></div>
-<div class="intro">
-
-<img class="mascot" src="picture/TrueGrit.jpg" alt="UMBC Mascot" align="right">
-
-<div class="description"> Introduction </div>
-
-This project is an Advisor/Student website that will allow Advisors as well as Students create 
-their schedules with ease. Working on this project allowed us to work first hand with other students
-and allowed us to know where our weaknesses and strengths lay when working in a team of developers.
-Oh and by the way Nat, your code is really good. Like really really good. I can tell you put alot of time
-and effort into learning these languages and it shows. You're doing a good job! Like really really good. I can tell you put alot of time
-and effort into learning these languages and it shows. You're doing a good job! Like really really good. I can tell you put alot of time
-and effort into learning these languages and it shows. You're doing a good job! Like really really good. I can tell you put alot of time
-and effort into learning these languages and it shows. You're doing a good job!
-
-</div>
-
-<div class="content">
-<div class="description"> Project Description </div>
-This portion will give a brief explanation of our project and how its used. It will allow the user to not
-be confused and to have an expectation going in when using this. This portion will give a brief explanation of our project and how its used. It will allow the user to not
-be confused and to have an expectation going in when using this. This portion will give a brief explanation of our project and how its used. It will allow the user to not
-be confused and to have an expectation going in when using this. This portion will give a brief explanation of our project and how its used. It will allow the user to not
-be confused and to have an expectation going in when using this.
-</div>
-
-
-<!--
-Name: Nathaniel Baylon
-Date:03/21/2015
-Class: CMSC331
-Project:Advisor Time Selection
-File: Proj2Tail.html
--->
-
-</body>
-</html>
+<?php
+$_SESSION['lastPage'] = "index.php";
+include('Proj2Tail.html');
+?>
